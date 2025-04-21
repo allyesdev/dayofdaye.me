c1c2374a81267e862e70aec3225f9e426900faeb
allyes13 <kimallyes13@gmail.com>
2025-04-20 23:51:15 -0400

    fix: chat router

---
+++ src/app/[locale]/chat/page.tsx
@@ -0,0 +1,11 @@
+const chat = () => {
+  return (
+    <div className='w-full h-full '>
+      <div className='flex flex-col items-center w-full h-full pt-20'>
+        <h1>Chat</h1>
+      </div>
+    </div>
+  );
+};
+
+export default chat;
